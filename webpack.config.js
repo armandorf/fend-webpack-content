82c69ee6de62cf0180c7c0273e81eb7a8c0750aa
Pedro Flores <armandorf.05@gmail.com>
2019-09-25 11:07:20 -0700

    Install babel dev dependencies: @babel/core, @babel/preset-env, and babel-loader Add configuration file .babelrc And add module rule to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -3,4 +3,20 @@ const webpack = require('webpack');
 
 module.exports = {
   entry: './src/client/index.js',
+  // introduction of loaders
+  module: {
+    // 1st rule we see in Webpack
+    rules: [
+      {
+        test: '/\.js$/', // anything that ends in ".js"
+        exclude: /node_modules/,
+        loader: "babel-loader" // running files above through this loader
+      }
+    ]
+  },
+  output: {
+    filename: 'main.js',
+    publicPath: '/dist'
+  }
+  // here "output" property could be added, but default is being used implicitly
 };
